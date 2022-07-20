dd17ec1eba98de624a7214de5f027ca58ff2b7d6
israelHCPontes <israelhcpontes@gmail.com>
2022-07-20 02:01:00 -0300

    STYLE(TELA01, TELA02)

---
+++ src/componentes/Main.js
@@ -0,0 +1,19 @@
+import Tela01 from "./Tela01";
+
+export default function Main(){
+    return(
+       <Tela01/>
+        // <>
+        // <div class="tela02">
+        //      <div className="topoAPP">
+        //         <img src="./arquivosulteis/img/image 1.svg"/>
+        //        <div class="textoTopo"><h1>ZapRecall</h1></div>
+        //     </div>
+        //     <button className="pergunta">
+        //         <h2>Pergunta 1</h2> 
+        //         <ion-icon name="play-outline"></ion-icon>
+        //     </button>
+        // </div>
+        // </>
+    )
+}
---
+++ src/componentes/Tela01.js
@@ -0,0 +1,14 @@
+
+
+export default function Tela01(){
+    return (
+        <div className="tela01">
+            <div className="logo">
+                <img src="./arquivosulteis/img/image 1.svg" />
+            </div>
+            <h1>ZapRecall</h1>
+            <button>Iniciar Recall!</button>
+        </div>
+    
+    ) 
+}
