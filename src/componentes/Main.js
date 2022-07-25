8127cd0937db9323ea1eecec6c4c7398d4f74949
Israel_Pontes <101795771+IsraelHCPontes@users.noreply.github.com>
2022-07-25 17:10:08 -0300

    Update Main.js

--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -3,7 +3,8 @@ import Tela01 from "./Tela01";
 import Tela02 from "./Tela02";
 
 export default function Main(){
-
+  DadosDeckJSX.sort(() => Math.random() - 0.5)
+  
   const [clikado, setClickado] = React.useState(true) 
 
   return (<>{clikado ? <Tela01 estate={clikado} setClickado={setClickado}/> : <Tela02/>}</>)
